18589568f8268f142e2403a1ca3a655b6746ee2a
Yuras <uraspokhrel25@gmail.com>
2026-07-10 11:43:30 +0545

    Getting Product Details from Json

--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
+import AxiosInstance from '@/lib/AxiosInstance'
 
 const ProductDetails = () => {
-    const paramsData = useParams()
-console.log(paramsData.id)
+  const { id } = useParams()
+
+  const [product, setProduct] = useState([])
+
+  const getProduct = async () => {
+    const axiosResponse = await AxiosInstance.get(`/posts/${id}`)
+    setProduct([axiosResponse.data])
+  }
+
+  useEffect(() => {
+    getProduct()
+  }, [])
+
   return (
     <div>
-        <h1>User Details Page</h1>
-        <p>ID: {paramsData.id}</p>
-      
+      <h1>Product Details Page</h1>
+
+      {product.map((item) => (
+        <div key={item.id}>
+          <p><strong>Title:</strong> {item.title}</p>
+          <p><strong>ID:</strong> {item.id}</p>
+          <p><strong>Body:</strong> {item.body}</p>
+          <p><strong>User ID:</strong> {item.userId}</p>
+        </div>
+      ))}
     </div>
   )
 }
